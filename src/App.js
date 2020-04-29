06cbcbe521e268c85ae2d6e8687873cb978e04c1
Vadertin-1104 <ernestinemoreno4@gmail.com>
2020-04-29 13:19:38 -0700

    API data loaded

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
+// eslint-disable-next-line
+import { Route, Link } from "react-router-dom";
 import ArtistList from './components/ArtistList';
+// eslint-disable-next-line
 import LocationList from './components/LocationList';
 
+
 function App() {
+
+
+
   return (
     <div>
       <h1>Bliss</h1>
       <ArtistList />
       <LocationList />
     </div>
-  );
+ );
+
 }
+
+
 
 export default App;
--- src/components/ArtistList.js
+++ src/components/ArtistList.js
@@ -1,14 +1,28 @@
 import React from 'react';
 
-function ArtistList() {
+export default class ArtistList extends React.Component {
+
+
+async componentDidMount() {
+// eslint-disable-next-line 
+const artistUrl = `https://api.songkick.com/api/3.0/search/artists.json?apikey=${process.env.REACT_APP_SONGKICK_API_KEY}&query=slander
+`;  
+// eslint-disable-next-line
+const response = await fetch(artistUrl);
+// eslint-disable-next-line
+const data = await response.json();
+    console.log(artistUrl)
+    console.log(response)
+    console.log(data)
+}
+ 
+render() {
     return (
         <form className='form-horizontal'>
-            <input placeholder='Search By Artist Name' type='text' name='searchString' required />
+        <input placeholder='Search By Artist Name' type='text' name='searchString' required />
         <button type='submit'>Go!</button>    
-
-        </form>      
-
-  ) 
+        </form>           
+  )  
 }
 
-export default ArtistList;
+}
