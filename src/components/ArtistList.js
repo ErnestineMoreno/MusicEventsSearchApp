81d56810a1981565d97226315d6bc51902390c27
Vadertin-1104 <ernestinemoreno4@gmail.com>
2020-04-29 20:39:11 -0700

    added data by metro area

--- src/components/ArtistList.js
+++ src/components/ArtistList.js
@@ -1,26 +1,27 @@
 import React from 'react';
 
-export default class ArtistList extends React.Component {
+export default class LocationList extends React.Component {
 
 
 async componentDidMount() {
 // eslint-disable-next-line 
-const artistUrl = `https://api.songkick.com/api/3.0/search/artists.json?apikey=${process.env.REACT_APP_SONGKICK_API_KEY}&query=slander
+const locationUrl = `https://api.songkick.com/api/3.0/search/artists.json?apikey=${process.env.REACT_APP_SONGKICK_API_KEY}&query=slander
 `;  
     
 // eslint-disable-next-line
-const response = await fetch(artistUrl);
+const response = await fetch(locationUrl);
 // eslint-disable-next-line
 const data = await response.json();
-    console.log(artistUrl)
+    console.log(locationUrl)
     console.log(response)
     console.log(data)
+    console.log(data.resultsPage.results.artist)
 }
  
 render() {
     return (
         <form className='form-horizontal'>
-        <input placeholder='Search By Artist Name' type='text' name='searchString' required />
+        <input placeholder='Search By City' type='text' name='searchString' required />
         <button type='submit'>Go!</button>    
         </form>           
   )  
--- src/components/LocationList.js
+++ src/components/LocationList.js
@@ -1,13 +1,30 @@
 import React from 'react';
 
-function LocationList() {
-   return (
-        <form className='form-horizontal'>
-            <input placeholder='Search By Location' type='text' name='searchString' required />
-        <button type='submit'>Go!</button>    
-        </form>      
+export default class L extends React.Component {
 
-  ) 
+
+async componentDidMount() {
+// eslint-disable-next-line 
+const artistUrl = `https://api.songkick.com/api/3.0/search/locations.json?query=Los_Angeles&apikey=${process.env.REACT_APP_SONGKICK_API_KEY}
+`;  
+    
+// eslint-disable-next-line
+const response = await fetch(artistUrl);
+// eslint-disable-next-line
+const data = await response.json();
+    console.log(artistUrl)
+    console.log(response)
+    console.log(data.resultsPage.results.location)
+    
+}
+ 
+render() {
+    return (
+        <form className='form-horizontal'>
+        <input placeholder='Search By Artist Name' type='text' name='searchString' required />
+        <button type='submit'>Go!</button>    
+        </form>           
+  )  
 }
 
-export default LocationList;
+}
